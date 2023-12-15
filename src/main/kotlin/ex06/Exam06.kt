c98d1ccaf020d7f238f20ffbda125bbc32c9dcaa
이호준 <psh10066@gmail.com>
2023-12-15 17:14:04 +0900

    클래스 예제

---
+++ src/main/kotlin/ex06/Exam06.kt
@@ -0,0 +1,55 @@
+package org.example.ex06
+
+fun main() {
+
+    // new 키워드 없이 생성자 호출
+    val dog = Dog("해피")
+
+    // 보편적으로 getter, setter 없이 직접 property 접근
+    println(dog.age)
+    dog.age = 20
+    println(dog.age)
+
+    println(dog.weight)
+
+    dog.eat()
+    dog.bark()
+}
+
+interface Bark {
+    fun bark()
+}
+
+interface Temp {
+    fun hi()
+}
+
+abstract class Animal(
+    private val name: String
+) : Bark {
+    fun eat() {
+        println("$name 식사 시작")
+    }
+}
+
+// extends, implements 구분 없이 : 로 상속받는다.
+class Dog(
+    private val name: String,
+    var age: Int = 0
+) : Animal(name), Temp {
+
+    // getter, setter의 기본 구조는 아래와 같으나, 기본적으로 세팅됨
+    var weight: Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    override fun bark() {
+        println("$name : 멍멍")
+    }
+
+    override fun hi() {
+        println("$name : hi")
+    }
+}
